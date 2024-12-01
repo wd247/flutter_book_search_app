6e3525a2ff4f302ff7fdbe0c971cafd739c65121
wd247 <junny3344@gmail.com>
2024-12-01 21:35:14 +0900

    sparta_book_search

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(),
+        ),
+        body: Text('HomePage'),
+      ),
+    );
+  }
+}
